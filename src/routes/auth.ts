b39188b0963e483ec4a482cd71a3b7c9d7df8e97
Will Boyce <me@willboyce.com>
2019-06-11 11:44:42 +0100

    auth: add `auth/v1/public-keys/:username endpoint for devices ssh auth
    Change-type: minor
    Signed-off-by: Will Boyce <will@balena.io>

---
+++ src/routes/auth.ts
@@ -0,0 +1,29 @@
+import { RequestHandler } from 'express';
+
+import { resinApi } from '../platform';
+import { handleHttpErrors } from '../platform/errors';
+
+export const getUserPublicKeys: RequestHandler = (req, res) => {
+	const { username } = req.params;
+
+	if (username == null) {
+		return res.send(400);
+	}
+
+	return resinApi
+		.get({
+			resource: 'user__has__public_key',
+			options: {
+				$select: 'public_key',
+				$filter: { user: { username } },
+			},
+			passthrough: { req },
+		})
+		.then((data: Array<{ public_key: string }>) => {
+			const authorizedKeys = data.map(e => e.public_key).join('\n');
+			res.status(200).send(authorizedKeys);
+		})
+		.catch(err => {
+			handleHttpErrors(req, res, err);
+		});
+};
